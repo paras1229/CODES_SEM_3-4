262fe7bc64efe7cb16e790111723d833b2ddb710
Paras Thumar <paras21thumar@gmail.com>
2025-12-15 19:10:30 +0530

    Add files via upload

---
+++ JAVA sem 4/6_ternary.java
@@ -0,0 +1,11 @@
+/* code no. 6 */
+import java.util.*;
+class codition
+{
+        public static void main(String[] args)
+        {
+                int a=10, b=20, max;
+                max=(a>b)? a:b;
+                System.out.println(max);
+        }
+}
