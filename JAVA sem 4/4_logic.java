e21c866a090cc414d67c17ca6dc2b7e08a39b8c4
Paras Thumar <paras21thumar@gmail.com>
2025-12-16 19:37:16 +0530

    Add files via upload

---
+++ JAVA sem 4/4_logic.java
@@ -0,0 +1,11 @@
+/* code no. 4 */
+import java.util.*;
+class logical
+{
+        public static void main(String[] args)
+        {
+                System.out.println((10>5) && (2>1));
+                System.out.println((10>5) || (2<1));
+                System.out.println(!(10>5));
+        }
+}
